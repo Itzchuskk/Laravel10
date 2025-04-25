96900c0c1f3b5587a11889bf3ea8f28e91134ea3
Itzchuskk <al02897748@tecmilenio.mx>
2025-04-25 10:34:52 -0600

    ACTIVITY 15

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\GenderController;
 use App\Http\Controllers\UniverseController;
 use App\Http\Controllers\GeneralController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
 
 
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
 
 Route::get('/dashboard', function () {
@@ -29,7 +31,41 @@ Route::middleware('auth')->group(function () {
     
         Route::resource('universes', UniverseController::class);
     
-    
+        Route::get('/', function () {
+            return view('welcome');
+        });
+        Route::get('/form', function () {
+            return view('form');
+        });
+        
+        Route::post('/upload', [FileController::class, 'upload'])->name('upload');
+        Route::post('/download', [FileController::class, 'download'])->name('download');
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+        
+        Route::middleware('auth')->group(function () {
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        
+        
+                Route::resource('superheroes', SuperHeroController::class);
+        
+                Route::resource('gender', GenderController::class);
+            
+                Route::resource('universes', UniverseController::class);
+                Route::get('/form', function () {
+                    return view('form');
+                });
+                
+                Route::post('/upload', [FileController::class, 'upload'])->name('upload');
+            
+            
+        });
+
+
+
     
 });
 
